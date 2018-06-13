a2f05204d5757bef21b22c461e969c1dbe7fbab9
Ryan Birmingham <birm@rbirm.us>
2018-06-13 17:45:41 -0400

    less wrong version

--- camicData.js
+++ camicData.js
@@ -4,26 +4,26 @@ var { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 var { makeExecutableSchema } = require('graphql-tools');
 
 var data = {
-  slides: [],
-  collections: [],
-  markings: [],
-  marktypes: [],
+  slides: [{id: 1, name: "bob", location: "/dev/null"}],
+  collections: [{id: 1, name:"everything", type: "slide", contents:[1]}],
+  markings: [{id: 1, marktype: 1, geometry:[[[1,1], [0,1], [0,0], [1,0]]] }],
+  marktypes: [{id: 1, slide: 1, name: "box"}],
   templates: [],
   overlays: []
 }
 
 var typeDefs = `
 type Collection {
-  id: ID!
+  id: Int!
   name: String
   type: String!
-  contents: [ID!]
+  contents: [Int!]
 }
 
 type Slide{
-  id: ID!
+  id: Int
   name: String
-  location: String!
+  location: String
   mpp: Float
   checksum: String
 }
@@ -34,14 +34,14 @@ type Feature{
 }
 
 type Marking{
-  id: ID!
-  marktype: ID!
+  id: Int!
+  marktype: Int!
   features: [Feature]
 }
 
 type Marktype{
-  id: ID!
-  slide: ID!
+  id: Int!
+  slide: Int!
   type: String
   name: String!
 }
@@ -53,26 +53,25 @@ type Question{
 }
 
 type Template{
-  id: ID!
+  id: Int!
   name: String
   type: String
   questions: [Question]
 }
 
 type Overlay{
-  id: ID!
+  id: Int!
   path: String
   name: String
 }
 
 type RootQuery{
-  Template:Template
-  Marktype: Marktype
-  Marking: Marking
-  Feature: Feature
-  Slide: Slide
-  Collection: Collection
-  Overlay: Overlay
+  Slides: [Slide]
+  Collections: [Collection]
+  Templates: [Template]
+  Overlays: [Overlay]
+  Markings: [Marking]
+  Marktypes: [Marktype]
 }
 
 schema{
@@ -81,11 +80,18 @@ schema{
 `;
 
 var resolvers = {
+  RootQuery: {
+    Slides: ()=>data.slides,
+    Templates: ()=>data.templates,
+    Overlays: ()=>data.overlays,
+    Markings: ()=>data.markings,
+    Marktypes: ()=>data.marktypes
+  },
   Collection: {
     id: (root, args) => data.collections.filter((x)=>x.id==args[0])
   },
   Slide: {
-    id: (root, args) => data.slides.filter((x)=>x.id==args[0])
+    id: (root, args) => console.log("hi")
   },
   Template: {
     id: (root, args) => data.templates.filter((x)=>x.id==args[0])
