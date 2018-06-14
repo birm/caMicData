293f0c29f589d3242756bd97d9b39a2fa1ef7459
Ryan Birmingham <birm@rbirm.us>
2018-06-14 11:44:52 -0400

    connectors

--- camicData.js
+++ camicData.js
@@ -2,6 +2,121 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 var { makeExecutableSchema } = require('graphql-tools');
+import Mongoose from 'mongoose';
+mongoose.Promise = global.Promise;
+
+const mongo = Mongoose.connect('mongodb://localhost/views', (err) => {
+  if(err){
+    console.error('Could not connect to MongoDB on port 27017');
+  }
+});
+
+var Connectors = {};
+
+Connectors.Collection = mongoose.model('Collection',{
+  id: { type: Number, required: true, unique: true },
+  name: String,
+  type: String,
+  contents: Array
+});
+
+Connectors.Slide= mongoose.model('Slide',{
+  id: { type: Number, required: true, unique: true },
+  name: String,
+  location: String,
+  mpp: Number,
+  checksum: String
+});
+
+Connectors.Marking= mongoose.model('Marking',{
+  id: { type: Number, required: true, unique: true },
+  marktype: { type: Number, required: true},
+  features: Array
+});
+
+Connectors.Marktype= mongoose.model('Marktype',{
+  id: { type: Number, required: true, unique: true },
+  slide: { type: Number, required: true},
+  type: String,
+  name: String
+});
+
+Connectors.Template= mongoose.model('Template',{
+  id: { type: Number, required: true, unique: true },
+  name: String,
+  type: String,
+  questions: Array
+});
+
+Connectors.Overlay== mongoose.model('Overlay',{
+  id: { type: Number, required: true, unique: true },
+  path: String,
+  name: String
+});
+
+const Collections{
+  getAll(){
+    return []
+  }
+  getById(){
+    return[]
+  }
+  addNew(x){
+    return []
+  }
+};
+const Slides{
+  getAll(){
+    return []
+  }
+  getById(){
+    return[]
+  }
+  addNew(x){
+    return []
+  }
+};
+const Markings{
+  getAll(){
+    return []
+  }
+  getById(){
+    return[]
+  }
+};
+const Marktypes{
+  getAll(){
+    return []
+  }
+  getById(){
+    return[]
+  }
+  addNew(x){
+    return []
+  }
+};
+const Templates{
+  getAll(){
+    return []
+  }
+  getById(){
+    return[]
+  }
+  addNew(x){
+    return []
+  }
+};
+const Overlays{
+  getAll(){
+    return []
+  }
+  getById(){
+    return[]
+  }
+  addNew(x){
+    return []
+  }
+};
 
 var data = {
   slides: [{id: 1, name: "bob", location: "/dev/null"}],
@@ -74,18 +189,28 @@ type RootQuery{
   Marktypes: [Marktype]
 }
 
+type RootMutation {
+  newSlide(input:Slide): Slide
+  newCollection(input:Collection): Collection
+  newTemplate(input: Template): Template
+  newOverlay(input:Overlay): Overlay
+  newMarking(input:Marking): Marking
+  newMarktype(input:Marktype): Marktype
+}
+
 schema{
   query: RootQuery
+  mutation: RootMutation
 }
 `;
 
 var resolvers = {
   RootQuery: {
-    Slides: ()=>data.slides,
-    Templates: ()=>data.templates,
-    Overlays: ()=>data.overlays,
-    Markings: ()=>data.markings,
-    Marktypes: ()=>data.marktypes
+    Slides: ()=>Collections.getAll(),
+    Templates: ()=>Templates.getAll(),
+    Overlays: ()=>Overlays.getAll(),
+    Markings: ()=>Markings.getAll(),
+    Marktypes: ()=>Marktypes.getAll()
   },
   Collection: {
     id: (root, args) => data.collections.filter((x)=>x.id==args[0])
