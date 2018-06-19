f45c6e24595e9f8eba0742c7f7dd9130c97e2c89
Ryan Birmingham <birm@rbirm.us>
2018-06-19 11:07:16 -0400

    allow search on oid

--- components/Controllers.js
+++ components/Controllers.js
@@ -1,10 +1,19 @@
 var Connector = require('./Connector.js');
 var Validation = require('./Validation.js');
-var Joi = require('joi')
+var Joi = require('joi');
+var ObjectId = require('mongodb').ObjectID;
 
 // generic methods to post and get data from a collection with validation
 
 var controllers = {}
+
+function id_fix(query){
+  if (query['id']){
+    query["_id"] = new ObjectId(query['id']);
+    delete query['id'];
+  }
+  return query
+}
 
 controllers.findMethod = function(collection) {
     return function(request, response) {
