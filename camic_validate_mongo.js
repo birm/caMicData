d941b3a168f594f1b9ba47358c4c2d722be8dc93
Ryan Birmingham <birm@rbirm.us>
2018-07-17 16:26:51 -0400

    Create camic_validate_mongo.js

---
+++ camic_validate_mongo.js
@@ -0,0 +1,170 @@
+db.createCollection("collection", {
+  validator: {
+     $jsonSchema: {
+       bsonType: "object",
+        required: ["name", "type", "contents"],
+        properties: {
+           name: {
+              bsonType: "string",
+              description: "must be a string and is required"
+           },
+           type: {
+              enum: ["slide", "collection"]
+              description: "can be a collection of slides or of collections"
+           },
+           contents: {
+              bsonType: "array",
+              description: "must have contents"
+           }
+         }
+     }
+ }
+});
+
+db.createCollection("slide", {
+   validator: {
+      $jsonSchema: {
+        bsonType: "object",
+         required: ["name", "location", "mpp"],
+         properties: {
+            name: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            location: {
+               bsonType: "string",
+               description: "must be a string and is required"
+            },
+            mpp: {
+               bsonType: "number",
+               minimum: 0,
+               description: "must be a string and is required"
+            }
+          }
+      }
+  }
+});
+
+db.createCollection("heatmap", {
+  validator: {
+     $jsonSchema: {
+       bsonType: "object",
+        required: [ "slide", "name", "width", "height", "key", "values" ],
+        properties: {
+           name: {
+              bsonType: "string",
+              description: "must be a string and is required"
+           },
+           slide: {
+              bsonType: "string",
+              description: "must be a string and is required"
+           },
+           height: {
+              bsonType: "number",
+              minimum: 0,
+              description: "must be a number and is required"
+           },
+           width: {
+              bsonType: "number",
+              minimum: 0,
+              description: "must be a number and is required"
+           },
+           key: {
+              bsonType: "string",
+              description: "must be a string and is required"
+           },
+           values: {
+              bsonType: "array",
+              description: "must be an array of array of values"
+           }
+         }
+     }
+ }
+});
+
+db.createCollection("marktype", {
+  validator: {
+     $jsonSchema: {
+       bsonType: "object",
+        required: [ "name", "slide", "type"],
+        properties: {
+           name: {
+              bsonType: "string",
+              description: "must be a string and is required"
+           },
+           slide: {
+              bsonType: "string",
+              description: "must be a string and is required"
+           },
+           type: {
+              bsonType: "string",
+              description: "must be a string and is required"
+           }
+         }
+     }
+ }
+});
+
+db.createCollection("template", {
+  validator: {
+     $jsonSchema: {
+       bsonType: "object",
+        required: [ "name", "type", "questions"],
+        properties: {
+           name: {
+              bsonType: "string",
+              description: "must be a string and is required"
+           },
+           type: {
+              bsonType: "string",
+              description: "must be a string and is required"
+           },
+           questions: {
+              bsonType: "array",
+              items: {
+                bsonType: "object",
+                required: [ "field", "type", "enum"],
+                properties: {
+                  type: {
+                     bsonType: "string",
+                     description: "must be a string and is required"
+                  },
+                  field: {
+                     bsonType: "string",
+                     description: "must be a string and is required"
+                  },
+                  "enum": {
+                     bsonType: "array",
+                     description: "must be a string and is required"
+                  },
+                }
+              }
+              description: "jsonform question objects"
+           },
+         }
+     }
+ }
+});
+
+db.createCollection("overlay", {
+  validator: {
+     $jsonSchema: {
+       bsonType: "object",
+        required: [ "name", "path", "slide"],
+        properties: {
+           name: {
+              bsonType: "string",
+              description: "must be a string and is required"
+           },
+           path: {
+              bsonType: "string",
+              description: "must be a string and is required"
+           },
+           slide: {
+              bsonType: "string",
+              description: "must be a string and is required"
+           }
+         }
+     }
+ }
+});
